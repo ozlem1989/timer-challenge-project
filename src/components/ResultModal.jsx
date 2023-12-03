27b04864dc2efee0f43685240d95446a40784262
ozlem.engin <ozlem.engin@turkcell.com.tr>
2023-12-03 22:15:13 +0300

    ref,portal added to pages.

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,43 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  return ReactDOM.createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score : {score}</h2>}
+
+      <p>
+        The target time was <strong>{targetTime} seconds</strong>
+      </p>
+
+      <p>
+        You stopped the timer with
+        <strong> {formattedRemainingTime} seconds left </strong>
+      </p>
+      <form method="dialog">
+        <button onClick={onReset}>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
+
+export default ResultModal;
